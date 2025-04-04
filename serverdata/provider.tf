4632145fa55cb80dd7653af604db743ebad0ba96
urwashi-lav <ulavsare@gmail.com>
2025-04-04 15:55:39 +0530

    file shifted to folder

---
+++ serverdata/provider.tf
@@ -0,0 +1,12 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.42.0"
+    }
+  }
+}
+provider "aws" {
+  alias = "us-east-1"
+  region = "us-east-1"
+ }
---
+++ serverdata/variable.tf
@@ -0,0 +1,10 @@
+variable "ami_id" {
+  description = "The AMI ID to use for the instance"
+  type        = string
+}
+
+variable "instance_type" {
+  description = "The instance type to use"
+  type        = string
+  default     = "t2.micro"
+}
